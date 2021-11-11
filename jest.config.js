93809c41d5c8871cd60aab73b3e7565a3976a532
Renato Santos <renatops1991@gmail.com>
2021-11-11 07:47:45 -0300

    feat: ensure SignUpController returns 400 if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    // eslint-disable-next-line comma-dangle
     '.+\\.ts$': 'ts-jest',
-    // eslint-disable-next-line comma-dangle
   },
-  // eslint-disable-next-line semi
 };
