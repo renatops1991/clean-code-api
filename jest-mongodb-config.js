d38e8c8cbb443b862cc15cf40a7ee2a52c94242c
Renato Santos <renatops1991@gmail.com>
2021-11-21 20:53:56 -0300

    rafactor: move mongodb methods to a helper

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -2,11 +2,13 @@
 /* eslint-disable comma-dangle */
 module.exports = {
   mongodbMemoryServerOptions: {
+    instance: {
+      dbName: 'jest',
+    },
     binary: {
-      version: '4.0.3',
+      version: '4.2.0',
       skipMD5: true,
     },
     autoStart: false,
-    instance: {},
   },
 };
