1d0db8c76d48b581f6fd131d0f6b73e8d4a2ff75
Renato Santos <renatops1991@gmail.com>
2021-12-27 15:51:40 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,24 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log-mongo-repository'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  it('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('foo')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
