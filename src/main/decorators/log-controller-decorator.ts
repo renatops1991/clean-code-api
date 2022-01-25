b99552ec96149e33549aabaafca185cfc1c71a09
Renato Santos <renatops1991@gmail.com>
2022-01-25 08:42:07 -0300

    refactor: change correct path of the imports

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -1,6 +1,6 @@
 
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller
