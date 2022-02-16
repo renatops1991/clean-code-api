48867a879c05f011f1a0a00a288266401e59517f
Renato Santos <renatops1991@gmail.com>
2022-02-16 08:01:42 -0300

    refactor: add ObjectId as type of updateAccessToken method

--- src/data/protocols/db/update-access-token-repository.ts
+++ src/data/protocols/db/update-access-token-repository.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from 'mongodb'
 
 export interface UpdateAccessTokenRepository{
-  updateAccessToken(id: ObjectId, token: string): Promise<void>
+  updateAccessToken(id: string | ObjectId, token: string): Promise<void>
 }
