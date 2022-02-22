35406a131f4617a7db24af683ad8230575ba4e26
Renato Santos <renatops1991@gmail.com>
2022-02-22 07:41:15 -0300

    refactor: change path in import file in routes file

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,11 @@
+/* eslint-disable node/no-path-concat */
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(`${__dirname}` + '/../routes').map(async file => {
+  readdirSync(`${__dirname}/../routes`).map(async file => {
     if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
