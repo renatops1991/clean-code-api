65174780dd4fd785c0da8d63adbb78d1f93aad8a
Renato Santos <renatops1991@gmail.com>
2022-03-03 00:08:45 -0300

    feat: ensure DbAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,13 +1,17 @@
-import { AddAccount, AccountModel, AddAccountModel, Hasher, AddAccountRepository } from './db-add-account-protocols'
+import { AddAccount, AccountModel, AddAccountModel, Hasher, AddAccountRepository, LoadAccountByEmailRepository } from './db-add-account-protocols'
 export class DbAddAccount implements AddAccount {
   private readonly hasher: Hasher
   private readonly addAccountRepository: AddAccountRepository
-  constructor (hasher: Hasher, addAccountRepository: AddAccountRepository) {
+  constructor (
+    hasher: Hasher,
+    addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {
     this.hasher = hasher
     this.addAccountRepository = addAccountRepository
   }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     const hashPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashPassword }))
     return account
