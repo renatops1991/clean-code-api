8836374d65d6b98283d551d9de755aebb85136d2
Renato Santos <renatops1991@gmail.com>
2022-03-05 08:31:55 -0300

    feature: ensure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,14 @@
+
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAnswer[]
+}
+
+export interface SurveyAnswer{
+  answer: string
+  image?: string
+}
+
+export interface AddSurvey {
+  add(data: AddSurveyModel): Promise<void>
+}
