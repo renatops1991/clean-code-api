f6bc0884c48b6e56ad864962567da57bd03755e8
Renato Santos <renatops1991@gmail.com>
2022-03-09 08:02:30 -0300

    refactor: move AuthMiddleware dependecies to a protocols file

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,9 +2,7 @@
 import { forbidden, serverError, success } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
-import { AccountModel } from '../../domain/models/account'
-import { HttpRequest } from '../protocols'
+import { LoadAccountByToken, AccountModel, HttpRequest } from './auth-middleware-protocols'
 
 interface SutTypes {
   sut: AuthMiddleware
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,7 +1,12 @@
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+
 import { AccessDeniedError } from '../errors'
 import { forbidden, serverError, success } from '../helpers/http/http-helper'
-import { HttpRequest, HttpResponse, Middleware } from '../protocols'
+import {
+  LoadAccountByToken,
+  HttpRequest,
+  HttpResponse,
+  Middleware
+} from './auth-middleware-protocols'
 
 export class AuthMiddleware implements Middleware {
   constructor (
