cdaf8e4b790bd7c499d412eb4928519e82526c41
Renato Santos <renatops1991@gmail.com>
2022-03-09 07:38:56 -0300

    test: ensure AuthMiddleware returns 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -28,15 +28,16 @@ const makeLoadAccountByToken = (): LoadAccountByToken => {
       return await new Promise(resolve => resolve(makeFakeAccount()))
     }
   }
-
   return new LoadAccountByTokenStub()
 }
 
 const makeSut = (): SutTypes => {
   const loadAccountByTokenStub = makeLoadAccountByToken()
   const sut = new AuthMiddleware(loadAccountByTokenStub)
-
-  return { sut, loadAccountByTokenStub }
+  return {
+    sut,
+    loadAccountByTokenStub
+  }
 }
 
 describe('AuthMiddleware', () => {
@@ -45,11 +46,17 @@ describe('AuthMiddleware', () => {
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
-
   it('Should call LoadAccountByToken with correct accessToken', async () => {
     const { sut, loadAccountByTokenStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('accessToken')
+  })
+  it('Should return 403 if LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load')
+      .mockResolvedValueOnce(new Promise(resolve => resolve(null)))
+    const expectedResponse = await sut.handle({})
+    expect(expectedResponse).toEqual(forbidden(new AccessDeniedError()))
   })
 })
