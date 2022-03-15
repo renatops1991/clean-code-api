fe17bcfbe3e80402e7c3e720c93781ca94c1cd26
Renato Santos <renatops1991@gmail.com>
2022-03-14 23:52:40 -0300

    feat: ensure add survery route 403 if no access token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: request.headers
+    }
+    const httpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(request, httpResponse.body)
+      next()
+    }
+    response.status(httpResponse.statusCode).json({
+      status: httpResponse.statusCode,
+      message: httpResponse.body.message
+    })
+  }
+}
--- src/main/factories/usescases/account/db-account-factory.ts
+++ src/main/factories/usescases/account/db-account-factory.ts
@@ -1,10 +1,21 @@
+
 import { DbAddAccount } from '../../../../data/usecases/account/db-add-account'
+import { DbLoadAccountByToken } from '../../../../data/usecases/account/db-load-account-by-token'
 import { AddAccount } from '../../../../domain/usecases/add-account'
+import { LoadAccountByToken } from '../../../../domain/usecases/load-account-by-token'
 import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../config/env'
 
 export const makeDbAddAccount = (): AddAccount => {
   const bcryptAdapter = new BcryptAdapter(12)
   const accountMongoRepository = new AccountMongoRepository()
   return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
+}
+
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(jwtAdapter, accountMongoRepository)
 }
