2d7b8aa1d714a47e6c88dbc57444a4a054341d51
Renato Santos <renatops1991@gmail.com>
2022-03-25 12:05:52 -0300

    refactor: remove user roles from authentication middleware

--- src/main/middlewares/authentication.ts
+++ src/main/middlewares/authentication.ts
@@ -2,4 +2,4 @@ import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-export const userAuth = adaptMiddleware(makeAuthMiddleware('user'))
+export const userAuth = adaptMiddleware(makeAuthMiddleware())
