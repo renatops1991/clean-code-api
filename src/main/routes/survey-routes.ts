0421b4a73f428bce2868da7e01da4b9155cf6d1b
Renato Santos <renatops1991@gmail.com>
2022-03-25 11:36:47 -0300

    refactor: move authenticate user to a separeted files

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController, makeLoadSurveysController } from '../factories/controllers/survey/survey-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { adminAuth, userAuth } from '../middlewares/authentication'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const userAuth = adaptMiddleware(makeAuthMiddleware('user'))
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
   router.get('/surveys', userAuth, adaptRoute(makeLoadSurveysController()))
 }
