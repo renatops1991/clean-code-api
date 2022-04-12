d50ff6c213b3fa16316174ff7d5c1d409bc17517
Renato Santos <renatops1991@gmail.com>
2022-04-12 08:59:55 -0300

    test: ensure save survey result returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,34 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
+import env from '@/main/config/env'
+import { Collection } from 'mongodb'
 import request from 'supertest'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const password = await hash('123', 12)
+  const account = await accountCollection.insertOne({
+    name: 'john foo bar',
+    email: 'john@bar.com',
+    password
+  })
+  const id = account.insertedId
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
 
 describe('SaveSurveyResult Routes', () => {
   beforeAll(async () => {
@@ -8,6 +36,14 @@ describe('SaveSurveyResult Routes', () => {
   })
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveysResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('UpdateSurveyResult', () => {
@@ -18,6 +54,28 @@ describe('SaveSurveyResult Routes', () => {
           answer: 'foo'
         })
         .expect(403)
+    })
+    it('Should return 200 on success', async () => {
+      const accessToken = await makeAccessToken()
+      const survey = await surveyCollection.insertOne({
+        question: 'foo',
+        answers: [{
+          answer: 'foo',
+          image: '/image/foo.jpg'
+        }, {
+          answer: 'xis'
+        }],
+        createdAt: new Date()
+      })
+
+      const surveyId = await surveyCollection.findOne({ _id: survey.insertedId })
+      await request(app)
+        .put(`/api/surveys/${surveyId._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'foo'
+        })
+        .expect(200)
     })
   })
 })
