cd299bced744e024b736e2061e8873f9594a2fdc
Renato Pereira <renatops1991@gmail.com>
2022-05-18 08:56:39 -0300

    feat: add swagger docs

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import express from 'express'
+import setupSwagger from './swagger'
 
 const app = express()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,7 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean code Node Api',
+    description: 'Api for creating surveys between programmers'
+  }
+}
