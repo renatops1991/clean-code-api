31c8a59cb5727163e1750111d86eea7894a48ccd
Renato Pereira <renatops1991@gmail.com>
2022-06-19 21:15:52 -0300

    ensure DbLoadSurveyResult throw exception if LoadSurveyResultRepository throws exception

--- src/data/usecases/survey-result/test/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/test/db-load-survey-result.spec.ts
@@ -1,6 +1,7 @@
 import { DbLoadSurveyResult } from '../db-load-survey-result'
 import { LoadSurveyResultRepository } from '../db-survey-result-protocols'
 import { mockLoadSurveyResultRepository } from '@/data/mocks'
+import { throwError } from '@/domain/fixtures'
 
 type SutTypes = {
   sut: DbLoadSurveyResult
@@ -23,5 +24,12 @@ describe('DbLoadSurveyResult UseCase', () => {
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('foo')
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('foo')
+  })
+
+  it('Should throw exception if LoadSurveyResultRepository throw exception', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
+    const expectedError = sut.load('foo')
+    await expect(expectedError).rejects.toThrow()
   })
 })
