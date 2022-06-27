b0d0806c567983a1528b98502bc26990fa215588
Renato Pereira <renatops1991@gmail.com>
2022-06-27 16:27:33 -0300

    feat: ensure LoadSurveyResultController return 200 on success

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError, success } from '@/presentation/helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
@@ -21,8 +21,8 @@ export class LoadSurveyResultController implements Controller {
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
-      await this.loadSurveyResult.load(surveyId)
-      return null
+      const surveyResult = await this.loadSurveyResult.load(surveyId)
+      return success(surveyResult)
     } catch (error) {
       return serverError(error)
     }
