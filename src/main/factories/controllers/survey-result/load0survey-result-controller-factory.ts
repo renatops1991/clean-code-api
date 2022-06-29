1f88b1c031f33a9590d353c480d55971f3d9d729
Renato Pereira <renatops1991@gmail.com>
2022-06-29 08:55:35 -0300

    feat: ensure load survey route returns 403 if no accessToken is provided

---
+++ src/main/factories/controllers/survey-result/load0survey-result-controller-factory.ts
@@ -0,0 +1,10 @@
+import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller'
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveyById } from '@/main/factories/usescases/survey/db-survey-factory'
+import { Controller } from '@/presentation/protocols'
+import { makeDbLoadSurveyResult } from '@/main/factories/usescases/survey-result/db-load-survey-result'
+
+export const makeLoadSurveyResultController = (): Controller => {
+  const controller = new LoadSurveyResultController(makeDbLoadSurveyById(), makeDbLoadSurveyResult())
+  return makeLogControllerDecorator(controller)
+}
--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,8 +1,10 @@
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result-controller-factory'
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load0survey-result-controller-factory'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { userAuth } from '@/main/middlewares/authentication'
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', userAuth, adaptRoute(makeSaveSurveyResultController()))
+  router.get('/surveys/:surveyId/results', userAuth, adaptRoute(makeLoadSurveyResultController()))
 }
