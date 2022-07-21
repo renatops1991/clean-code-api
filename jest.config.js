7004d6233af2379482d781b0a22345a3b117bfe0
Renato Pereira <renatops1991@gmail.com>
2022-07-21 08:20:43 -0300

    chore: update version

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
-    '^.+\\.ts?$': '@swc/jest'
+    '^.+\\.ts?$': ['@swc/jest']
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
