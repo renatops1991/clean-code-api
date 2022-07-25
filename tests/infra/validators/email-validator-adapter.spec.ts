d355b33d09e685d53a6914dee47cc6eee4d834fd
Renato Pereira <renatops1991@gmail.com>
2022-07-25 09:02:37 -0300

    refactor: move infra validators dependencies to a separete file

--- tests/infra/validators/email-validator-adapter.spec.ts
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from '../email-validator-adapter'
+import { EmailValidatorAdapter } from '@/infra/validators/email-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
