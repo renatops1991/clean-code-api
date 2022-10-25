b45f9b7508d18daddd7c21fa8f945dedbc73e111
Renato Pereira <renatops1991@gmail.com>
2022-10-24 23:39:15 -0300

    refactor: move declaration of dependencies to constructor

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -3,13 +3,10 @@ import { LogErrorRepository } from '@/data/protocols/db/log/log-error-repository
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
-  private readonly controller: Controller
-  private readonly logErrorRepository: LogErrorRepository
-
-  constructor (controller: Controller, logErrorRepository: LogErrorRepository) {
-    this.controller = controller
-    this.logErrorRepository = logErrorRepository
-  }
+  constructor (
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) { }
 
   async handle (request: any): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(request)
